6fb669b826a83f38a8f796c01bcdbf505d2c76b9
MohammadJozaa7 <36266902+MohammadJozaa7@users.noreply.github.com>
2018-05-13 13:59:31 +0300

    add bar

---
+++ familyApp/app/components/Bar.js
@@ -0,0 +1,293 @@
+//import react from react
+import React from 'react';
+//import element from reacr-native 
+import { StyleSheet, Text, View, Button, TextInput, TouchableOpacity, ScrollView, Image } from 'react-native';
+//import FormInput & Header from react native elements
+import { FormInput, Header ,Divider, CheckBox,} from 'react-native-elements';
+//import from navidate react drawer
+import Drawer  from 'react-native-drawer'
+//import createDrawerNavigator as DrawerNavigator from react navigation
+import { createDrawerNavigator } from 'react-navigation'
+//import  from native-base
+//import { Container, Content, Icon, Header, Body } from 'native-base'
+
+//import the icon from lirbary  one by one (each one library in react native icon)
+import Icon0 from 'react-native-vector-icons/FontAwesome';
+import Icon1 from 'react-native-vector-icons/Entypo';
+import Icon2 from 'react-native-vector-icons/Feather';
+import Icon3 from 'react-native-vector-icons/Ionicons';
+import Icon4 from 'react-native-vector-icons/MaterialCommunityIcons';
+import Icon5 from 'react-native-vector-icons/MaterialIcons';
+import Icon6 from 'react-native-vector-icons/Octicons';
+import Icon7 from 'react-native-vector-icons/Foundation';
+
+//import the file
+import Login from './app/components/Login';
+import Profile from './app/components/Profile'
+
+const UserTypeGenderText={
+  female:'Mother',
+  male:'Father',
+  child:'Child',
+}
+
+//export Home from the react componant
+export default class Home extends React.Component{
+  //the constructor
+  constructor(){
+    //super for ES6
+    super();
+    //all the data save before to can show in the bar
+    this.state={
+      //defult thing when change from data base change here 🙂
+      //female (Mother) / male (Father)/ child (Children)
+      userType:'female',
+      //from 100%
+      userProgress:'100',
+      //for how many child in family
+      userRanking:'2',
+      //the money still
+      restMoney:'1500',
+    };
+  }
+
+goToDrawerIcon(){
+  alert('goToDrawerIcon');
+  return (<fScreen/>)
+};
+goToDrawer(){
+  alert('goToDrawer');
+  return (<fScreen/>)
+};
+goToTasks(){
+  alert('goToTasks');
+  //this.props.navigation.openDrawer()
+};
+
+
+  //render
+  render() {
+    //what return
+    return (
+      <View style={styles.allPage}>
+        <View style={styles.barView}>
+          <Header
+            backgroundColor='#123456'
+            innerContainerStyles={styles.innerContainer}
+            outerContainerStyles={styles.outerContainer}
+
+            leftComponent={
+              <View  style={styles.leftComponent}>
+                <Icon0 onPress={()=>this.props.navigation.navigate('DrawerOpen')} style={{color:'#0bf5fb'}} name="bars" size={35}/>
+                <Text onPress={this.goToDrawer.bind(this)} style={styles.textUnderIcon}>Menu</Text>
+              </View>
+            }
+
+            centerComponent={
+              <View style={styles.centerComponentView}>
+                <View style={styles.firstCenterComponent}>
+                  <Icon0 name={this.state.userType} size={35} color="red"/>
+                  <Text style={styles.textUnderIcon}>
+                    {UserTypeGenderText[this.state.userType]}
+                  </Text>
+                </View>
+                
+                <View style={styles.firstCenterComponent}>
+                  <Text style={styles.textIconDone}>{this.state.userProgress}%</Text>
+                  <Text style={styles.textUnderIcon}>
+                    Progress
+                  </Text>
+                </View>
+                <View style={styles.centerComponent}>
+                  <Text style={styles.textIconRank}>{this.state.userRanking}</Text>
+                  <Text style={styles.textUnderIcon}>
+                    Rank
+                  </Text>
+                </View>
+                <View style={styles.lastCenterComponent}>
+                  <Text style={styles.textIconMoney}>{this.state.restMoney}</Text>
+                  <Text style={styles.textUnderIcon}>
+                    Money
+                  </Text>
+                </View>
+                              
+              </View>
+            }
+
+            rightComponent={
+              <View style={styles.rightComponent}>
+                <Icon6 onPress={this.goToTasks.bind(this)} style={{color:'#0bf5fb'}} name="checklist" size={35}/>
+                <Text onPress={this.goToTasks.bind(this)} style={styles.textUnderIcon}>Tasks</Text>
+              </View>
+            }
+          >
+          </Header>
+        </View>
+        <View style={styles.otherView}>
+        <MyApp open={true}/>
+        
+        </View>
+
+      </View>
+    );
+  }
+}
+
+const MyApp=createDrawerNavigator(
+  {
+    Finance:{
+      //path:'/',
+      screen:Login,  
+    },
+    Shortage:{
+     //path:'/sent',
+     screen:Login,  
+    },
+    'Family Events':{
+     //path:'/sent',
+     screen:Profile,  
+    },
+    Study:{
+     //path:'/sent',
+     screen:Profile,  
+    },
+    Exam:{
+     //path:'/sent',
+     screen:Profile,  
+    },
+    'User Information':{
+     //path:'/sent',
+     screen:Profile,  
+    },
+    Outside:{
+     //path:'/sent',
+     screen:Profile,  
+    },
+    Tasks:{
+     //path:'/sent',
+     screen:Profile,  
+    },
+    Logout:{
+     //path:'/sent',
+     screen:Profile,  
+    }
+  },
+  {
+    initialRouteName:'Tasks',
+    drawerPosition:'left',
+    drawarWidth:100,
+    contentOptions:{
+      activeTintColor:'red',
+    }
+  }
+);
+
+const styles = StyleSheet.create({
+  allPage: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'white',
+  },
+  barView: {
+    flexDirection: 'column',
+    marginTop:35,
+  },
+  innerContainer: {
+    //backgroundColor:'green',
+    flexDirection: 'row',
+    alignItems: 'center',
+    margin:-12,
+  },
+  leftComponent: {
+    flex:1,
+    flexDirection:'column',
+    justifyContent:'center',
+    marginLeft:5,
+    alignItems: 'center',
+  },
+  rightComponent: {
+    flex:1,
+    flexDirection:'column',
+    justifyContent:'center',
+    marginRight:5,
+    alignItems: 'center',
+
+  },
+  centerComponentView: {
+    flexDirection: 'row',
+    //backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  firstCenterComponent: {
+    marginRight:15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  centerComponent: {
+    marginRight:15,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  lastCenterComponent: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textUnderIcon: {
+    fontSize: 13,
+    textAlign:'center',
+    fontWeight: 'bold',
+    color:'white',
+  },
+  textIconDone: {
+    fontSize: 25,
+    textAlign:'center',
+    fontWeight: 'bold',
+    color:'#3cff00',
+    marginBottom:5,
+  },
+  textIconRank: {
+    fontSize: 30,
+    textAlign:'center',
+    fontWeight: 'bold',
+    color:'red',
+  },
+  textIconMoney: {
+    fontSize: 25,
+    textAlign:'center',
+    fontWeight: 'bold',
+    color:'#3cff00',
+    marginBottom:5,
+  },
+  otherView: {
+    flex:1,
+    flexDirection: 'column',
+    backgroundColor: 'green',
+    marginBottom:35,
+  },
+  d: {
+    backgroundColor: 'red',
+    marginTop:150,
+  },
+});
+
+
+/*
+
+
+<CheckBox
+  center
+  title='Click Here to Remove This Item'
+  iconRight
+  iconType='material'
+  checkedIcon='clear'
+  uncheckedIcon='add'
+  checkedColor='red'
+  checked={this.state.checked}
+  />
+
+
+
+
+*/
---
+++ familyApp/app/components/Home.js
@@ -0,0 +1,213 @@
+//import react from react
+import React from 'react';
+//import element from reacr-native 
+import { StyleSheet, Text, View,Button,TextInput,TouchableOpacity } from 'react-native';
+//import the icon from lirbary
+import Icon from 'react-native-vector-icons/FontAwesome';
+//import input from elent react native
+import { FormInput } from 'react-native-elements';
+import {DrawerNavigator } from 'react-navigation'
+
+//export this app from the react componant
+export default class LoginPrototype extends React.Component {
+  //the constructor
+  /*
+  constructor(){
+    //super for es6
+    super();
+    //all the data save before sent in state
+    this.state={
+      username:'',
+      password:'',
+      data:'it is dont change',
+    };
+  }
+  */
+  static navigationOptions = {
+    tabBarLabel:'Screen 1',
+    drawarIcon:()=>{
+      return(
+        <Icon
+          name='user'
+          size={23}
+          style={{color:'blue'}}
+        >
+        </Icon>
+      );
+    }
+  }
+  //function to fecth data
+  login(){
+    alert(this.state.username+this.state.password);
+  }
+  signUp(){
+    alert('LEARN MORE IS WORK');
+  }
+
+  //render
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.container2}>
+  
+          <Text style={styles.mainText}>Login or Sign Up</Text>
+        
+          <View style={styles.usernamePasswordView}>
+            <Icon style={styles.usernamePasswordIcon} name="user" size={20} color="#000"/>
+            <TextInput style={styles.usernamePasswordTextInput}
+              placeholder="Username"
+              onChangeText={(username) => {this.setState({username})}}
+            />
+          </View>
+  
+          <View style={styles.usernamePasswordView}>
+            <Icon style={styles.usernamePasswordIcon} name="lock" size={20} color="#000"/>
+            <TextInput
+              type="password" 
+              maxlength="4" 
+              size="4"
+              style={styles.usernamePasswordTextInput}
+              placeholder="Password"
+              onChangeText={(password) => {this.setState({password})}}
+              secureTextEntry={true}
+            />
+          </View>
+
+
+          <Button
+            style={styles.loginScreenButton}
+            onPress={()=>this.props.navigation.navigate('DrawerOpen')}
+            title='HERE MUSt work 1'
+            >
+          </Button>
+
+          <TouchableOpacity
+            style={styles.loginScreenButton}
+            onPress={()=>this.props.navigation.navigate('DrawerOpen')}
+            underlayColor='red'>
+            <Text style={styles.loginSingUpText}>Go to press</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.loginScreenButton}
+            onPress={this.login.bind(this)}
+            underlayColor='red'>
+            <Text style={styles.loginSingUpText}>Login</Text>
+          </TouchableOpacity>
+      
+          <TouchableOpacity
+            style={styles.signUpScreenButton}
+            onPress={this.signUp.bind(this)}
+            underlayColor='blue'>
+            <Text style={styles.loginSingUpText}>Sign Up</Text>
+          </TouchableOpacity>
+        </View>
+    
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container2: {
+    flexDirection: 'column',
+    marginTop:35,
+    marginBottom:35,
+    backgroundColor: 'white',
+    //justifyContent: 'center',
+    //alignItems: 'center',
+  },
+  mainText: {
+    color:'black',
+    textAlign:'center',
+    paddingLeft : 10,
+    paddingRight : 10,
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  usernamePasswordView: {
+    flexDirection: 'row',
+    backgroundColor: 'red',
+    marginRight:20,
+    marginLeft: 20,
+    marginTop:10,
+    //size:20,
+    //position:'fixed',
+  },
+  usernamePasswordIcon: {
+    marginLeft: 10,
+    marginTop: 10,
+  },
+  usernamePasswordTextInput:{
+    marginLeft: 50,
+    color:'black',
+    height: 40,
+
+  },
+  loginScreenButton:{
+    marginRight:40,
+    marginLeft:40,
+    marginTop:10,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#1E6738',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  signUpScreenButton:{
+    marginRight:40,
+    marginLeft:40,
+    marginTop:10,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#1E6738',
+    borderRadius:10,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+    loginSingUpText: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingRight: 30,
+    paddingLeft: 30,
+    color: 'black',
+    fontSize: 10,
+    fontWeight: 'bold',
+
+  },
+});
+
+
+/*
+() => navigate('HomeScreen')
+
+<FormInput style={styles.input}
+  //placeholder='INPUT WITH CUSTOM ICON'
+  leftIcon={
+    <Icon
+    name='user'
+      size={50}
+      color='black'
+    />
+  }
+/>
+type="text" name="pin" maxlength="4" size="4"
+
+
+
+
+ 
+
+
+
+
+
+*/
