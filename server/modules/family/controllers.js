0e8597ab89e9d49638751f5a08045704606d9a36
Waed93 <alshyabwaed@gmail.com>
2018-05-14 09:18:04 +0300

    database connection

--- server/modules/family/controllers.js
+++ server/modules/family/controllers.js
@@ -1,73 +1,105 @@
 var models=require('./models');
-var request = require('request');
+// var request = require('request');
 var bcrypt = require('bcrypt');
 
 
 // this function will be invoked from the router /signUp
 
-// exports.createUser = function (req, res) {
-// 	var newUser={
+// exports.signupUser = function (req, res) {
+
+//   	var newUser={
 // 	email:req.body.email,
-// 	name:req.body.name,
+// 	name:req.body.username,
 
 // 	//need to encrypt the password first
 // 	password:req.body.password,
-// 	bDate:req.body.bData,
+// 	bDate:req.body.DateOfBirth,
 // 	familyId:req.body.familyId
 // 	}
-
+//     //console.log(newUser)
 // 	new models.User(newUser).save(function(err){
-// 		console.log("sucess creating a new user");
+		
 // 		res.send(newUser);
 // 	})
 // };
 
 
 exports.signupUser = function(req, res) {
-  var username = req.body['states[username]'];
-  var email = req.body['states[email]'];
-  var password = req.body['states[password]'];
-  var DateOfBirth = req.body['states[DateOfBirth]'];
-  console.log(User,"user")
+ 
 
-  User.findOne({ email: email },function(err,found){
 
-   if (!found ){
-     var newUser = new User({
-      username:username,
-      email: email,
-      password: password,
-      DateOfBirth: DateOfBirth
-
-    });
-     bcrypt.hash(password, 10, function(err, hash) {
-      newUser.password=hash;
-      newUser.save(function(err,obj) {
-       if(err){
-        res.status(500).send(err);
-        console.log("error")
-      }
-      else{
-        res.status(201).send("Thank You");
-      }
-    })
-    });
-       
-     }
-     else{
-      res.status(201).send("")
-    }
-  })
+ var newUser={
+  username : req.body.user.username,  
+   email :req.body.user.email,   
+   password :req.body.user.password,   
+   bdate :req.body.user.bdate,   
+   role :req.body.user.role,   
+   rank :req.body.user.rank,
+  familyId :req.body.user.familyId,
 }
+  
+  new models.User(newUser).save(function(err){
+    if(err){
+      res.send({msg:err})
+    }
+
+    else{
+      res.send({msg:"create a new user"});
+    }
+
+  })
+    
+    }
+
+
+  // models.User.findOne({ email: email },function(err,found){
+
+   // if (!found ){
+   //   var newUser =new models.User({
+   //    username:username,
+   //    email: email,
+   //    password: password,
+   //    DateOfBirth: DateOfBirth,
+   //    role:role,
+   //    rank:rank,
+   //    familyId:familyId
+   //  });
+   //   bcrypt.hash(password, 10, function(err, hash) {
+   //    newUser.password=hash;
+   //    newUser.save(function(err,obj) {
+   //     if(err){
+   //      res.status(500).send(err);
+   //      res.send("error");
+   //    }
+   //    else{
+   //      res.status(201).send("Thank You");
+   //      res.send("success signup");
+   //    }
+   //  })
+   //  });
+       
+   //   }
+   //   else{
+   //    res.status(201).send("")
+   //  }
+  // })
+
+
+
+
+
+
+
+
 exports.signinUser = function(req, res) {
   var email = req.body['states[email]'];
   var passWord = req.body['states[password]'];
-  User.findOne({ email: email },function(err,user){
+  models.User.findOne({ email: email },function(err,user){
    if (!user ){
     console.log("user not exist")
   } else {
     var data="kk";
-    User.comparepassword(password,user.password, function(err,match) {
+    models.User.comparepassword(password,user.password, function(err,match) {
       if (match) {
         data="coreeeeect";
         res.status(201).send(data);
--- server/modules/family/routes.js
+++ server/modules/family/routes.js
@@ -4,18 +4,22 @@ const appControllers=require('./controllers');
 
 
 appRouter.route('/api/signup').post((req,res)=>{
-
 	// to retreive all family members
-	console.log("a post request is received")
+	console.log("request received",req.body)
 	appControllers.signupUser(req,res);
-})
-
-
-
-appRouter.route('/api/login').post((req,res)=>{
-    //to check the password for the user and start the session 
-    appControllers.signinUser(req,res);
 
 })
 
+
+
+// appRouter.route('/api/login').post((req,res)=>{
+//     //to check the password for the user and start the session 
+//     appControllers.signinUser(req,res);
+
+// })
+// appRouter.route('/api/login').get((req,res)=>{
+//     //to check the password for the user and start the session 
+//     console.log('work please')
+
+// })
 module.exports=appRouter;
