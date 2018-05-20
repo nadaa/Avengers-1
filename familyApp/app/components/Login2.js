42a560885fda7e804538febc136cdd1bbc63692e
MohammadJozaa7 <36266902+MohammadJozaa7@users.noreply.github.com>
2018-05-21 02:17:13 +0300

    the basic work now start see the session

--- familyApp/app/components/Login2.js
+++ familyApp/app/components/Login2.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, KeyboardAvoidingView, TouchableOpacity,} from 'react-native';
 import axios from 'axios';  
-//import { createStackNavigator } from 'react-navigation';
 import SignUp from './SignUp';
 import Drawer from './Drawer';
-
 export default class Login2 extends React.Component {
   constructor(props){
     super(props);
@@ -13,91 +11,37 @@ export default class Login2 extends React.Component {
       password:'1234567',
     }
   }
-  sendLogin(){
-  const { navigate } = this.props.navigation;
-  axios.post('http://192.168.1.82:3000/login2', {
-    user:this.state
-  })
+sendLogin(){
+  const {navigate}=this.props.navigation;
+  axios.post('http://192.168.1.82:3000/login2', {user:this.state})
   .then(function (response) {
     if(response.data.msg==="success login"){
       //alert(response.data.msg);
       navigate('Drawer');
-    }else if(response.data.msg==="the password is not correct"){
-     alert("the password is not correct please inser it correct");
-    }else if(response.data.msg==="no account"){
-      alert('You dont have an account please make one then login');
-      navigate('SignUp');
     }
   })
-  .catch(function (error) {
-    console.log(error);
-    alert(error);
-  });
 };
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <KeyboardAvoidingView behaviour='padding' style ={styles.wrapper}>
-        <View style={styles.container}>
-          <Text style={styles.header}> LOGIN </Text>
-          <TextInput style={styles.textInput} placeholder='Username'
-            value={this.state.username}
-            onChangeText={(username) => this.setState({username})} 
-          /> 
-            <TextInput
-              style={styles.textInput} 
-              placeholder='Password'
-              value={this.state.password}
-              onChangeText={(password) => this.setState({password})}  
-            /> 
-          <TouchableOpacity
-            style={styles.btn}
-            onPress={
-              this.sendLogin.bind(this)
-              //  navigate('Profile')
-            }
-          >
-            <Text>Log in</Text>
-          </TouchableOpacity>
-          <Text style={{color: 'blue', paddingTop:10}}
-            onPress={() =>  navigate('SignUp')}>
-            create account for free -SignUp-
-          </Text>
-        </View>
-      </KeyboardAvoidingView>
+<View style={styles.container}>
+  <Text style={styles.header}> LOGIN </Text>
+  <TextInput style={styles.textInput} placeholder='Username'
+  value={this.state.username} onChangeText={(username)=>this.setState({username})}/> 
+  <TextInput style={styles.textInput} placeholder='Password'
+  value={this.state.password} onChangeText={(password)=>this.setState({password})}/> 
+  <TouchableOpacity style={styles.btn} onPress={this.sendLogin.bind(this)}>
+  <Text>LOGIN</Text></TouchableOpacity>
+</View>
     );
   }
 }
-
 const styles = StyleSheet.create({
-  wrapper: {
-    flex: 1,
-  },
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#123456',
-    paddingLeft: 40,
-    paddingRight: 40,
-  },
-  header: {
-    fontSize: 24,
-    marginBottom: 60,
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-  textInput: {
-    alignSelf: 'stretch',
-    padding: 16,
-    marginBottom: 20,
-    backgroundColor: '#fff',
-  },
-  btn: {
-    alignSelf:'stretch',
-    backgroundColor:'#01c853',
-    padding:20,
-    alignItems:'center',
-  }
+  wrapper: {flex: 1},
+  container: {flex: 1,alignItems: 'center',justifyContent: 'center',
+  backgroundColor: '#123456',paddingLeft: 40,paddingRight: 40},
+  header: {fontSize: 30,marginBottom: 60,color: '#fff',fontWeight: 'bold'},
+  textInput: {alignSelf: 'stretch',padding: 16,marginBottom: 20,backgroundColor: '#fff'},
+  btn: {alignSelf:'stretch',backgroundColor:'#01c853',padding:20,alignItems:'center'}
 })
 
