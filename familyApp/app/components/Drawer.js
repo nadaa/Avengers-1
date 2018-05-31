d59d553d25039fc9e514dc2fc019b41b67dff6d2
hmtirawi <hmtirawi@gmail.com>
2018-05-31 17:56:06 +0300

    (feat): add eslint rules for Drawer page

--- familyApp/app/components/Drawer.js
+++ familyApp/app/components/Drawer.js
@@ -1,50 +1,45 @@
 import React from 'react';
-import { View, StyleSheet,Text } from 'react-native';
-import { createDrawerNavigator } from 'react-navigation'
-import Tasks from './Tasks';
+import { View, StyleSheet } from 'react-native';
+import { createDrawerNavigator } from 'react-navigation';
 import Finance from './Finance';
 import UserInfo from './UserInfo';
-import Bar from './Bar';
 import Shortage from './Shortage';
 import TaskMonitor from './TaskMonitor';
-import TasksDisplay from './TasksDisplay';
-import Login from './Login';
-import SignUp from './SignUp';
 
-const MyDrawer=createDrawerNavigator(
+const MyDrawer = createDrawerNavigator(
   {
-    'Task Monitor':{
-      screen:TaskMonitor,
+    'Task Monitor': {
+      screen: TaskMonitor,
     },
-    Finance:{
-      screen:Finance,
+    Finance: {
+      screen: Finance,
     },
-    Shortage:{
-      screen:Shortage,
+    Shortage: {
+      screen: Shortage,
     },
-    'User Information':{
-      screen:UserInfo,
+    'User Information': {
+      screen: UserInfo,
     },
   },
   {
-    initialRouteName:'Task Monitor',
-    drawerPosition:'left',
-    drawerWidth:200,
-    drawerOpenRoute:'DrawerOpen',
-    drawerCloseRoute:'DrawerClose',
-    drawerToggleRoute:'DrawerToggle',
-    contentOptions:{
-      activeTintColor:'green',
+    initialRouteName: 'Task Monitor',
+    drawerPosition: 'left',
+    drawerWidth: 200,
+    drawerOpenRoute: 'DrawerOpen',
+    drawerCloseRoute: 'DrawerClose',
+    drawerToggleRoute: 'DrawerToggle',
+    contentOptions: {
+      activeTintColor: 'green',
     },
   }
 );
 
-export default class Drawer extends React.Component{
+export default class Drawer extends React.Component {
   static router = MyDrawer.router;
-  render(){
-    return(
+  render() {
+    return (
       <View style={styles.allPage}>
-        <MyDrawer navigation={this.props.navigation}/>
+        <MyDrawer navigation={this.props.navigation} />
       </View>
     );
   }
@@ -55,6 +50,6 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'column',
     backgroundColor: 'white',
-    marginBottom:25,
+    marginBottom: 25,
   },
 });
