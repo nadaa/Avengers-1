2f4b4b8343d65821e9e96bd1f5decb7a143ea372
hmtirawi <hmtirawi@gmail.com>
2018-05-31 17:41:50 +0300

    (feat): add eslint rules

--- familyApp/App.js
+++ familyApp/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import {createStackNavigator} from 'react-navigation';
+import { createStackNavigator } from 'react-navigation';
 import Login from './app/components/Login';
 import SignUp from './app/components/SignUp';
 import Drawer from './app/components/Drawer';
@@ -13,32 +12,32 @@ import Shortage from './app/components/Shortage';
 import UserInfo from './app/components/UserInfo';
 import Finance from './app/components/Finance';
 
-const Application=  createStackNavigator({
-  SignUp:{screen:SignUp},
+const Application = createStackNavigator({
+  SignUp: { screen: SignUp },
   
-  Login:{ screen: Login },
-  'User Information':{screen:UserInfo},
+  Login: { screen: Login },
+  'User Information': { screen: UserInfo },
   Drawer: { screen: Drawer },
   DrawerKids: { screen: DrawerKids },
-  'Task Monitor':{screen:TaskMonitor},
-  Tasks:{screen:Tasks},
-  TasksDisplay:{screen:TasksDisplay},
-  },{
+  'Task Monitor': { screen: TaskMonitor },
+  Tasks: { screen: Tasks },
+  TasksDisplay: { screen: TasksDisplay },
+  }, {
   navigationOptions: {
     header: null,
   }
 });
 export default class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    // global.ip='http://10.0.2.2:3000/api'; //nada
-    // global.ip='http://192.168.8.105:3000/api'; //jozaa
-    global.ip='http://192.168.1.111:3000/api' //Waed
-    //global.ip='http://192.168.202.2:3000/api'; //Hussein
+    // global.ip = 'http://10.0.2.2:3000/api'; //nada
+    // global.ip = 'http://192.168.8.105:3000/api'; //jozaa
+    // global.ip = 'http://192.168.1.111:3000/api' //Waed
+    global.ip = 'http://192.168.202.2:3000/api'; //Hussein
   }
-  render(){
-    return(
-     <Application/>
+  render() {
+    return (
+     <Application />
     );
   }
 }
